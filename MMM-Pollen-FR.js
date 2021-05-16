30a10c943ced53140a2cd5f2d95abd3a9a8190d3
lekesako <lekesako@outlook.com>
2021-05-16 15:23:05 +0200

    add level in config

--- MMM-Pollen-FR.js
+++ MMM-Pollen-FR.js
@@ -8,6 +8,7 @@ Module.register("MMM-Pollen-FR", {
         updateInterval: 12 * 60 * 60 * 1000, // every 12 hours
         region_code: 59,
         fadeSpeed: 2000,
+		minLevel: 3
     },
 
     start: function() {
@@ -22,6 +23,7 @@ Module.register("MMM-Pollen-FR", {
 
     // Override dom generator.
     getDom: function() {
+		
         console.log("MMM-Pollen-FR : starting generate DOM ...");
         var wrapper = document.createElement("pollen");
 
@@ -35,13 +37,22 @@ Module.register("MMM-Pollen-FR", {
 
         //header row
         var tbl = document.createElement("table");
+		var tr = document.createElement("tr");
+		var td1 = document.createElement("td");
+		td1.innerHTML = " " 
+		var td2 = document.createElement("td");
+		td2.innerHTML = this.result.countyNumber + " - " +  this.result.countyName
+		tr.appendChild(td1);
+        tr.appendChild(td2);
+        tbl.appendChild(tr);
+		
         var tr = document.createElement("tr");
         
         // level description
         var td1 = document.createElement("td");
         td1.className = "pollen-padding";
         var icon1 = document.createElement("span");
-        icon2.className = "fa fa-line-chart";
+        icon1.className = "fa fa-line-chart";
 
         // pollen
         var td2 = document.createElement("td");
@@ -61,11 +72,7 @@ Module.register("MMM-Pollen-FR", {
         console.log("MMM-Pollen-FR : starting generate DOM ...");
 
         if (this.result && this.result.risks) {
-            var tr = document.createElement("tr");
-            var td1 = document.createElement("td");
-            td1.innerHTML = this.result.riskLevel;
-            var td2 = document.createElement("td");
-            td2.innerHTML = this.countyName
+           
             
             tr.appendChild(td1);
             tr.appendChild(td2);
@@ -79,30 +86,30 @@ Module.register("MMM-Pollen-FR", {
 
                 if (level > 0) {
                     allergens.push(pollenName);
-                }
-
-                var td1 = document.createElement("td");
-                td1.innerHTML = level;
-                var td2 = document.createElement("td");
-                td2.innerHTML = allergens.join(", ");
-
-                if(level >= 5) {
-                	td2.className = "pollen-high";
-                } else if(level >= 4) {
-                	td2.className = "pollen-mediumhigh";
-                } else if(level >= 3) {
-                	td2.className = "pollen-medium";
-                } else if(level >= 2) {
-                	td2.className = "pollen-lowmedium";
-                } else if(level >= 1) {
-                	td2.className = "pollen-low";
-                }
-
-                var tr = document.createElement("tr");
-                tr.appendChild(td1);
-                tr.appendChild(td2);
                 
-                tbl.appendChild(tr);
+					var td1 = document.createElement("td");
+					td1.innerHTML = level;
+					var td2 = document.createElement("td");
+					td2.innerHTML = allergens.join(", ");
+
+					if(level >= 5) {
+						td2.className = "pollen-high";
+					} else if(level >= 4) {
+						td2.className = "pollen-mediumhigh";
+					} else if(level >= 3) {
+						td2.className = "pollen-medium";
+					} else if(level >= 2) {
+						td2.className = "pollen-lowmedium";
+					} else if(level >= 1) {
+						td2.className = "pollen-low";
+					}
+
+					var tr = document.createElement("tr");
+					tr.appendChild(td1);
+					tr.appendChild(td2);
+					
+					tbl.appendChild(tr);
+				}
             });
         }
 
